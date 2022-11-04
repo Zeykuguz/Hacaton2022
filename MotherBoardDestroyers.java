c28149fffa4dca1d5ed5284f461c0a4997558a2d
alexei <oldlifeerror@gmail.com>
2022-11-04 16:21:58 +0200

    Move to the wall. Format code

--- MotherBoardDestroyers.java
+++ MotherBoardDestroyers.java
@@ -1,4 +1,5 @@
 package motherboarddest;
+
 import robocode.*;
 import robocode.*;
 import robocode.Robot;
@@ -7,141 +8,139 @@ import java.awt.*;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-public class MotherBoardDestroyers extends Robot
-{
+public class MotherBoardDestroyers extends Robot {
 
-	double moveAmount;
-	double steps;
-	double bfHeight;
-	double bfWidth;
+    double moveAmount;
+    double steps;
+    double bfHeight;
+    double bfWidth;
 
-	final int stepsToMove = 50;
+    final int stepsToMove = 50;
 
-	boolean hasDamage = false;
+    boolean hasDamage = false;
 
-	boolean moveRight = true;
-	boolean fired = false;
+    boolean moveRight = true;
+    boolean fired = false;
 
-	double lastDistance = 0;
+    double lastDistance = 0;
 
-	double lastbearingFromGun = 1;
+    double lastbearingFromGun = 1;
 
-	boolean allowMove = true;
+    boolean allowMove = true;
 
-	public void run() {
-		this.setBodyColor(Color.black);
-		this.setGunColor(Color.black);
-		this.setRadarColor(Color.orange);
-		this.setBulletColor(Color.cyan);
-		this.setScanColor(Color.cyan);
+    public void run() {
+        this.setBodyColor(Color.black);
+        this.setGunColor(Color.black);
+        this.setRadarColor(Color.orange);
+        this.setBulletColor(Color.cyan);
+        this.setScanColor(Color.cyan);
 
-		this.steps = 0;
+        this.steps = 0;
 
-		this.bfHeight = this.getBattleFieldHeight();
-		this.bfWidth = this.getBattleFieldWidth();
+        this.bfHeight = this.getBattleFieldHeight();
+        this.bfWidth = this.getBattleFieldWidth();
 
-		this.moveAmount = Math.max(this.bfWidth, this.bfHeight);
+        this.moveAmount = Math.max(this.bfWidth, this.bfHeight);
 
-		this.turnLeft(this.getHeading() % 90.0D);
+        this.turnLeft(this.getHeading() % 90.0D);
+        this.ahead(this.moveAmount);
 
-		this.turnGunRight(360.0D);
-		this.ahead(this.moveAmount);
+        this.turnGunRight(360.0D);
 
-		this.setAdjustGunForRobotTurn(true);
+        this.setAdjustGunForRobotTurn(true);
 
-		while(true) {
-			this.move();
-		}
-	}
+        while (true) {
+            this.move();
+        }
+    }
 
-	public void move(){
-		if (!fired) {
-			turnGunRight(360);
-		}
-		int extraMove = 0;
+    public void move() {
+        if (!fired) {
+            turnGunRight(360);
+        }
+        int extraMove = 0;
 
-		if(allowMove) {
-			if (hasDamage) {
-				extraMove += 50;
-				//turnRight(90);
-				hasDamage = false;
-			}
+        if (allowMove) {
+            if (hasDamage) {
+                extraMove += 50;
+                hasDamage = false;
+            }
 
-			if (moveRight) {
-				this.ahead(this.stepsToMove + extraMove);
-			} else {
-				this.ahead(-this.stepsToMove - extraMove);
-			}
-			turnGunRight(20);
-			turnGunLeft(40);
-		}
+            if (moveRight) {
+                this.ahead(this.stepsToMove + extraMove);
+            } else {
+                this.ahead(-this.stepsToMove - extraMove);
+            }
+            turnGunRight(20);
+            turnGunLeft(40);
+        }
 
-		fired = false;
+        fired = false;
 
-	}
+    }
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Calculate exact location of the robot
-		double absoluteBearing = getHeading() + e.getBearing();
-		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Calculate exact location of the robot
+        double absoluteBearing = getHeading() + e.getBearing();
+        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 
-		turnGunRight(bearingFromGun);
-		double distance = e.getDistance();
+        turnGunRight(bearingFromGun);
+        double distance = e.getDistance();
 
-		if(lastDistance == 0){
-			lastDistance = distance;
-		}
+        if (lastDistance == 0) {
+            lastDistance = distance;
+        }
 
 
-		if(lastbearingFromGun == bearingFromGun){
-			allowMove = false;
-			fire(Math.abs(Math.min(5, getEnergy() - .1)));
-		}else {
-			if (distance < 300) {
-				fire(Math.abs(Math.min(2, getEnergy() - .1)));
-			} else if (distance < 450) {
-				fire(Math.abs(Math.min(1, getEnergy() - .1)));
-			} else if (distance < 600) {
-				fire(Math.abs(Math.min(0.5, getEnergy() - .1)));
-			} else {
-				fire(Math.abs(Math.min(0.25, getEnergy() - .1)));
-			}
-			allowMove = true;
-		}
+        if (lastbearingFromGun == bearingFromGun) {
+            allowMove = false;
+            fire(Math.abs(Math.min(5, getEnergy() - .1)));
+        } else {
+            if (distance < 300 ) {
+                fire(Math.abs(Math.min(2, getEnergy() - .1)));
+            } else if (distance < 450) {
+                fire(Math.abs(Math.min(1, getEnergy() - .1)));
+            } else if (distance < 600) {
+                fire(Math.abs(Math.min(0.5, getEnergy() - .1)));
+            } else {
+                fire(Math.abs(Math.min(0.25, getEnergy() - .1)));
+            }
+            allowMove = true;
+        }
 
-		lastbearingFromGun = bearingFromGun;
-		fired = true;
-		if(distance < 300 && lastDistance > distance ){
-			this.moveRight = false;
-		}else if(distance > 300){
-			this.moveRight = true;
-		}
+        lastbearingFromGun = bearingFromGun;
+        fired = true;
+        if (distance < 300 && lastDistance > distance) {
+            this.moveRight = false;
+        } else if (distance > 300) {
+            this.moveRight = true;
+        }
 
-		this.lastDistance = distance;
+        this.lastDistance = distance;
 
-		this.move();
-	}
+        this.move();
+    }
 
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		hasDamage = true;
-	}
+    /**
+     * onHitByBullet: What to do when you're hit by a bullet
+     */
+    public void onHitByBullet(HitByBulletEvent e) {
+        hasDamage = true;
+    }
 
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		this.steps = 0;
-		this.turnRight(90.0D);
-	}
+    /**
+     * onHitWall: What to do when you hit a wall
+     */
+    public void onHitWall(HitWallEvent e) {
+        this.steps = 0;
+        this.turnRight(90.0D);
+    }
 
-	public void onWin(WinEvent e) {
-		for(int i = 0; i < 50; ++i) {
-			this.turnRight(30.0D);
-			this.turnLeft(30.0D);
-		}
-	}
+    public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; ++i) {
+            this.turnRight(30.0D);
+            this.turnLeft(30.0D);
+        }
+    }
 }
